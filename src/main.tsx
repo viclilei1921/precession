b54a81a7f2cc8a89768b6957d94e7be23a2f424d
lilei <bernie@lednets.com>
2026-09-24 17:58:07 +0800

    feat: 未完成

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
-import './app/app.css';
+import './style/index.css';
 
 ReactDOM.createRoot(document.body).render(
   <React.StrictMode>
---
+++ src/router/path.ts
@@ -0,0 +1,12 @@
+export const PATH = {
+  /* 首页 */
+  home: '/',
+  /* 今天 */
+  today: '/today',
+  /* 计划 */
+  plan: '/plan',
+  /* 日记 */
+  journal: '/journal',
+  /* 日记 */
+  journalEditor: '/journal/$entryId'
+} as const;
